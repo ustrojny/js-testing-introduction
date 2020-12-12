2376da245101714c5b2072fa242728dcf7a31a7c
ustrojny <ustrojny@gmail.com>
2020-12-12 13:11:46 +0100

    add more tests

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 const { generateText, checkAndGenerate } = require('./util.js');
 
 test('should output name and age', () => {
@@ -13,4 +14,21 @@ test('should output data-less text', () => {
 test('should generate a valid text output', () => {
     const text = checkAndGenerate('White Wolf', 7);
     expect(text).toBe('White Wolf (7 years old)');
-})
+});
+
+test('should click around', async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 8,
+        args: ['--windows-size=1920,1080']
+    });
+    const page = await browser.newPage();
+    await page.goto('file:///Users/urszulastrojny/Downloads/PRIV%20ULA/CodersCamp/PROJECTS/js-testing-introduction-master/index.html');
+    await page.click('input#name');
+    await page.type('input#name', 'Wolfus');
+    await page.click('input#age');
+    await page.type('input#age', '2');
+    await page.click('#btnAddUser');
+    const finalText = await page.$eval('.user-item', el => el.textContent);
+    expect(finalText).toBe('Wolfus (2 years old)');
+}, 10000)
